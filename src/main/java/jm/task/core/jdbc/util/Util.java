65c5355c299d7f12eaf9ab094511443b70fe407a
DeezLink <alslv@mail.ru>
2023-05-05 11:23:20 +0300

    "PP114"

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -6,17 +6,24 @@ import java.sql.SQLException;
 
 public class Util {
     // настройка соеденения с БД
+
+    public static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
+    public static final String DB_CONNECTION = "jdbc:mysql://localhost:3306/mydb114";
+    public static final String DB_USER = "root";
+    public static final String DB_PASSWORD = "root";
+
     public static Connection getConnection() {
+
 
         Connection connection = null;
 
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb114", "root", "root");
+            Class.forName(DB_DRIVER);
+            connection = DriverManager.getConnection(DB_CONNECTION, DB_USER, DB_PASSWORD);
             System.out.println("Connection OK");
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
-            System.out.println("Connection ERROR");
+            System.err.println("Connection ERROR");
         }
         try {
             assert connection != null;
@@ -24,7 +31,14 @@ public class Util {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-        return connection;
 
+        return connection;
+    }
+    public void close() {
+        try {
+            getConnection().close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
